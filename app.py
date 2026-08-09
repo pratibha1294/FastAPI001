66497919964009470280f008257fee05c90acff7
Pratibha <50351997+pratibha1294@users.noreply.github.com>
2026-08-10 01:09:44 +0530

    Add login functionality and refactor request DTO class for user registration

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from repo import register
+from repo import register,login
+
 
 
 app = FastAPI()
@@ -13,12 +14,21 @@ def root():
 def health():
     return {"status": "ok"}
 
-class RegisterRequestDto(BaseModel):
+class AuthRequestDTO(BaseModel):
     email: str
     password: str
 
 @app.post("/register")
-def register_user(request: RegisterRequestDto):
+def register_user(request: AuthRequestDTO):
     register(request.email, request.password)
     # Here you would typically add logic to save the user to a database
     return {"message": "User registered successfully", "user": request}
+
+@app.post("/login")
+def login_user(request: AuthRequestDTO):
+    success = login(request.email,request.password)
+    print(success)
+    if success!= None: 
+        return {"is_success": True}
+    return {"is_success": False}
+
--- repo.py
+++ repo.py
@@ -5,3 +5,11 @@ def register(email, password):
     cursor.execute("""Insert into users (email,password,created_at, updated_at) 
     Values (%s,%s, NOW(), NOW())""", (email,password))
     connection.commit()
+
+def login(email,password):
+
+    cursor = connection.cursor()
+    cursor.execute("SELECT email, password FROM users where email= %s and password=%s", (email,password))
+
+    user = cursor.fetchone()
+    return user
