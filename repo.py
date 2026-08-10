9db082d26d5a97da2d2618d31e090b0f42579ee1
Pratibha <50351997+pratibha1294@users.noreply.github.com>
2026-08-10 17:36:23 +0530

    Refactor register function for improved readability and add validateUserPassword function

--- repo.py
+++ repo.py
@@ -3,7 +3,8 @@ from database import connection
 def register(email, password):
     cursor = connection.cursor()
     cursor.execute("""Insert into users (email,password,created_at, updated_at) 
-    Values (%s,%s, NOW(), NOW())""", (email,password))
+    Values (%s,%s, NOW(),
+    NOW())""", (email,password))
     connection.commit()
 
 def login(email,password):
@@ -13,3 +14,9 @@ def login(email,password):
 
     user = cursor.fetchone()
     return user
+
+def validateUserPassword(userId: int, password: str):
+    cursor = connection.cursor()
+    cursor.execute("SELECT count(userId) as matches FROM users where userId= %d and password= %s", (userId,password))
+    result= cursor.fetchone()
+    return result["matches"]==1
